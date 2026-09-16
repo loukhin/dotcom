2986d5916bdf0b91a3edc064cea6a787c77cad6e
Supakit Khawmeewong <loukhin@lop.town>
2026-09-16 10:42:31 +0700

    feat: new web again? in this economy?

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<header>
+    <div class="flex justify-between items-end mx-4 font-caveat">
+        <a href="/" class="text-4xl">Loukhin on the interweb</a>
+        <div class="flex gap-4">
+            <div class="group">
+                <span class="group-hover:opacity-0 transition-all duration-200 pointer-events-none">
+                    about me
+                </span>
+            </div>
+            <a href="https://github.com/loukhin">github</a>
+        </div>
+    </div>
+    <div class="separator w-full h-10"></div>
+</header>
+
+<div class="flex justify-center items-center grow">i'll write here someday</div>
